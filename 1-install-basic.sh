5a1a597298b409be582c6aed7ae32df30db4d8e9
rkeepr <rkeeper@proton.me>
2022-09-15 16:19:38 -0300

    simplify the scripts, added new packages

--- 1-install-basic.sh
+++ 1-install-basic.sh
@@ -1,26 +1,30 @@
 #!/bin/sh
 
-username=$(id -u -n 1000)
+user=$(id -u -n 1000)
 
 # update the system
 sudo pacman -Syu --noconfirm
 
 # minimal install
-sudo pacman -S --needed --noconfirm base-devel firefox pcmanfm xorg-server xorg-xinit libxinerama gnu-free-fonts libertinus-font pipewire gnome-keyring htop neofetch
+sudo pacman -S --needed --noconfirm base-devel firefox pcmanfm xorg-server xorg-xinit libxinerama gnu-free-fonts libertinus-font pipewire pipewire-pulse pulsemixer gnome-keyring htop neofetch
 
 # install yay (aur helper)
 git clone https://aur.archlinux.org/yay-bin.git
 cd yay-bin
 makepkg -si --needed --noconfirm
-
 #get out of yay-bin folder
 cd ..
 
-# install some useful packages
-yay -S --needed --noconfirm --batchinstall nerd-fonts-hack lf-bin librewolf-bin brave-bin downgrade
+# install some necessary packages from the aur
+yay -S --needed --noconfirm --batchinstall nerd-fonts-hack lf-bin
 
-# copy xinitrc file per archwiki instructions
-cp /etc/X11/xinit/xinitrc /home/$username/.xinitrc
+# check for xinitrc file, if not found copy the one in /etc
+cp /etc/X11/xinit/xinitrc /home/$user/.xinitrc
+[ ! -d "/home/$user/.xinitrc" ] && cp "/etc/X11/xinit/xinitrc" "/home/$user/.xinitrc"
 
 # make some folders
-mkdir /home/$username/.config
+mkdir /home/$user/downloads
+mkdir /home/$user/images
+mkdir /home/$user/videos
+# check if ~/.config folder exist. if not create it
+[ ! -d "/home/$user/.config" ] && mkdir -p "/home/$user/.config"
--- 2-yay-install.sh
+++ 2-yay-install.sh
@@ -4,10 +4,3 @@
 git clone https://aur.archlinux.org/yay-bin.git
 cd yay-bin
 makepkg -si --needed --noconfirm
-
-# get out of yay-bin folder and make another folder
-cd ..
-mkdir testfolder2
-
-# install some useful packages
-yay -S --needed --noconfirm --batchinstall nerd-fonts-hack lf-bin librewolf-bin brave-bin downgrade
