2c48258a2ddea4c4b8aef448e10e81981b6f4c46
rkeepr <rkeeper@proton.me>
2022-09-15 13:31:09 -0300

    more fixes, also testing

--- 1-install-basic.sh
+++ 1-install-basic.sh
@@ -13,3 +13,14 @@ cp /etc/X11/xinit/xinitrc /home/$username/.xinitrc
 
 # make some folders
 mkdir /home/$username/.config
+
+# install yay (aur helper)
+git clone https://aur.archlinux.org/yay-bin.git
+cd yay-bin
+makepkg -si --needed --noconfirm
+
+# test
+mkdir testfolder
+
+# install some useful packages
+yay -S nerd-fonts-hack lf-bin librewolf-bin brave-bin downgrade
--- 2-yay-install.sh
+++ 2-yay-install.sh
@@ -3,4 +3,10 @@
 # install yay (aur helper)
 git clone https://aur.archlinux.org/yay-bin.git
 cd yay-bin
-makepkg -sic --needed --noconfirm
+makepkg -si --needed --noconfirm
+
+# test
+mkdir testfolder2
+
+# install some useful packages
+yay -S nerd-fonts-hack lf-bin librewolf-bin brave-bin downgrade
