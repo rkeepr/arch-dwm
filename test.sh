7e4b4debfd49f1db22e37e4755c889d1e1c4fe4d
rkeepr <rkeeper@proton.me>
2022-09-15 11:01:10 -0300

    fix pacman

--- test.sh
+++ test.sh
@@ -3,18 +3,18 @@
 username=$(id -u -n 1000)
 
 # update the system
-pacman -Syu --no-confirm
+pacman -Syu --noconfirm
 
 # minimal install
-pacman -S --needed --no-confirm firefox pcmanfm xorg-server xorg-xinit libxinerama gnu-free-fonts libertinus-font pipewire gnome-keyring
+pacman -S --needed --noconfirm firefox pcmanfm xorg-server xorg-xinit libxinerama gnu-free-fonts libertinus-font pipewire gnome-keyring htop neofetch
 
 # copy xinitrc file per archwiki instructions
 cp /etc/X11/xinit/xinitrc /home/$username/.xinitrc
 
 # make some folders
-mkdir /home/$username/.config /home/$username/.config
+mkdir /home/$username/.config /home/$username/testdir
 
 # setup yay (aur helper)
 git clone https://aur.archlinux.org/yay.git
 cd yay
-makepkg -si --needed --no-confirm
+makepkg -si --needed --noconfirm
