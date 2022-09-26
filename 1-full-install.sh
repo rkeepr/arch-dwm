e8a5273bf88ed2d84b8c9f3f3f6a4a4414032bf1
rkeepr <rkeepr@proton.me>
2022-09-26 12:21:55 -0300

    remove ufw

--- 1-full-install.sh
+++ 1-full-install.sh
@@ -5,7 +5,7 @@ sudo pacman -S --needed --noconfirm reflector
 sudo reflector --latest 30 --sort rate --age 12 --protocol https --save /etc/pacman.d/mirrorlist
 
 ### full install
-sudo pacman -S --needed --noconfirm base-devel xorg-server xorg-xinit libxinerama firefox pcmanfm gvfs xarchiver unrar unzip zip gnu-free-fonts man-db htop ttf-liberation libertinus-font adobe-source-han-sans-otc-fonts adobe-source-han-serif-otc-fonts noto-fonts noto-fonts-emoji ttf-joypixels pipewire pipewire-pulse pulsemixer pamixer mpd mpc ncmpcpp mpv yt-dlp libdvdcss viewnior lxappearance qt5ct qt6ct xwallpaper redshift unclutter picom papirus-icon-theme materia-gtk-theme kvantum kvantum-theme-materia sxhkd neofetch trash-cli xclip xdotool reflector numlockx dunst slock bc lxtask moreutils bash-completion perl-file-mimeinfo xdg-user-dirs p7zip xcolor cuetools rxvt-unicode simple-scan xsane keepassxc ufw picard gimp imagemagick handbrake qbittorrent amule telegram-desktop libreoffice-fresh atril pdfarranger fcitx5 fcitx5-mozc fcitx5-configtool fcitx5-gtk fcitx5-qt gnome-keyring seahorse polkit-gnome xorg-xev xorg-xkill xorg-xprop xorg-xsetroot xorg-xwininfo virtualbox virtualbox-host-modules-arch linux-headers
+sudo pacman -S --needed --noconfirm base-devel xorg-server xorg-xinit libxinerama firefox pcmanfm gvfs xarchiver unrar unzip zip gnu-free-fonts man-db htop ttf-liberation libertinus-font adobe-source-han-sans-otc-fonts adobe-source-han-serif-otc-fonts noto-fonts noto-fonts-emoji ttf-joypixels pipewire pipewire-pulse pulsemixer pamixer mpd mpc ncmpcpp mpv yt-dlp libdvdcss viewnior lxappearance qt5ct qt6ct xwallpaper redshift unclutter picom papirus-icon-theme materia-gtk-theme kvantum kvantum-theme-materia sxhkd neofetch trash-cli xclip xdotool reflector numlockx dunst slock bc lxtask moreutils bash-completion perl-file-mimeinfo xdg-user-dirs p7zip xcolor cuetools rxvt-unicode simple-scan xsane keepassxc picard gimp imagemagick handbrake qbittorrent amule telegram-desktop libreoffice-fresh atril pdfarranger fcitx5 fcitx5-mozc fcitx5-configtool fcitx5-gtk fcitx5-qt gnome-keyring seahorse polkit-gnome xorg-xev xorg-xkill xorg-xprop xorg-xsetroot xorg-xwininfo virtualbox virtualbox-host-modules-arch linux-headers
 
 ### install yay (aur helper)
 cd ~/
@@ -64,14 +64,3 @@ sed -i -e "s/user/$user/g" "$HOME/.local/share/lf/marks"
 
 # modify redshift value in xinitrc with current location
 sed -i -e "s/replace-loc/$current_loc/" "$HOME/.xinitrc"
-
-### configure and enable ufw
-sudo ufw limit 22/tcp # ssh
-sudo ufw allow 80/tcp # http
-sudo ufw allow 443/tcp # https
-sudo ufw default deny incoming
-sudo ufw default allow outgoing
-sudo ufw allow qBittorrent
-sudo ufw enable
-sudo systemctl enable ufw.service
-sudo systemctl start ufw.service
