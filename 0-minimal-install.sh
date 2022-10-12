d468f530907c60fd030f3db7603c29eb18718844
rkeepr <rkeepr@proton.me>
2022-10-12 01:17:05 -0300

    added redshift config file and a script (.local/bin/chloc-redshift.sh) to change the current location for redshift

--- 0-minimal-install.sh
+++ 0-minimal-install.sh
@@ -58,9 +58,14 @@ cp -r resources/config/* $HOME/.config
 cp -r resources/local/* $HOME/.local
 cp -r resources/home/. $HOME
 
-# modify generic "user" for current user
+# modify flameshot's config file with current user
+user=$(id -u -n 1000)
 sed -i -e "s/user/$user/" "$HOME/.config/flameshot/flameshot.ini"
-sed -i -e "s/user/$user/g" "$HOME/.local/share/lf/marks"
 
-# modify redshift value in xinitrc with current location
-sed -i -e "s/replace-loc/$current_loc/" "$HOME/.xinitrc"
+# modify redshift's config file with current location
+CURL_LOC="$(curl -s https://location.services.mozilla.com/v1/geolocate?key=geoclue)"
+LAT=$(echo $CURL_LOC | awk '{print $3}' | tr -d ',')
+LON=$(echo $CURL_LOC | awk '{print $5}' | tr -d '},')
+
+sed -i -e "s/replace-lat/$LAT/" "$HOME/.config/redshift/redshift.conf"
+sed -i -e "s/replace-lon/$LON/" "$HOME/.config/redshift/redshift.conf"
