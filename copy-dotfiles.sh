4d75b64f3204bb66c4248720126e3cd19841872f
rkeepr <rkeepr@proton.me>
2022-11-06 18:55:34 -0300

    changed script to modify the whole line instead of the placeholder word

--- copy-dotfiles.sh
+++ copy-dotfiles.sh
@@ -22,5 +22,5 @@ CURL_LOC="$(curl -s https://location.services.mozilla.com/v1/geolocate?key=geocl
 LAT=$(echo $CURL_LOC | awk '{print $3}' | tr -d ',')
 LON=$(echo $CURL_LOC | awk '{print $5}' | tr -d '},')
 
-sed -i -e "s/replace-lat/$LAT/" "$HOME/.config/redshift/redshift.conf"
-sed -i -e "s/replace-lon/$LON/" "$HOME/.config/redshift/redshift.conf"
+sed -i -e "/lat/c\lat=$LAT" "$HOME/.config/redshift/redshift.conf"
+sed -i -e "/lon/c\lon=$LON" "$HOME/.config/redshift/redshift.conf"
