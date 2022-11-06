677dda3ff5de257f8c3aba7fd99df60c8bc66235
rkeepr <rkeepr@proton.me>
2022-11-06 20:34:55 -0300

    removed packages that i'm not using

--- install-aur-packages.sh
+++ install-aur-packages.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 
-yay -S --needed --noconfirm --sudoloop librewolf-bin brave-bin downgrade gallery-dl shntool flacon
+yay -S --needed --noconfirm --sudoloop librewolf-bin brave-bin downgrade shntool
