bb2e100d84b665e9780610cd8632bc3f5b4e617b
rkeepr <rkeepr@proton.me>
2022-11-08 22:58:46 -0300

    Revert "get dmenu directly from from suckless site, revert dwmblocks and added a line to remove .git folder"
    This reverts commit 52477fbea4fcbaed279bba0e32f739723e1392df.

--- install-window-manager.sh
+++ install-window-manager.sh
@@ -2,31 +2,25 @@
 
 [ ! -d "$HOME/.local/src" ] && mkdir -p "$HOME/.local/src"
 cd ~/.local/src
-
 # dwm
 git clone https://github.com/rkeepr/dwm.git
 cd "dwm"
 sudo make clean install
 cd ..
-
+# dmenu
+git clone https://github.com/rkeepr/dmenu.git
+cd "dmenu"
+sudo make clean install
+cd ..
 # st
 git clone https://github.com/rkeepr/st.git
 cd "st"
 sudo make clean install
 cd ..
-
-# dmenu
-curl -LO https://dl.suckless.org/tools/dmenu-5.2.tar.gz
-tar -x -f dmenu-5.2.tar.gz
-rm dmenu-5.2.tar.gz
-cd "dmenu-5.2"
-cp ~/arch-dwm/resources/dmenu/config.h .
-sudo make clean install
-cd ..
-
 # dwmblocks
-git clone https://github.com/torrinfail/dwmblocks.git
+curl -Lo dwmblocks.zip https://github.com/torrinfail/dwmblocks/archive/refs/heads/master.zip
+unzip dwmblocks.zip
+mv dwmblocks-master dwmblocks
+cp ~/arch-dwm/resources/dwmblocks/blocks.h dwmblocks
 cd "dwmblocks"
-rm -rf .git
-cp ~/arch-dwm/resources/dwmblocks/blocks.h .
 sudo make clean install
