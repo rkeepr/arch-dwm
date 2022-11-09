8cb73384217ee8e4064dde284b4d31df2c9d6b5a
rkeepr <rkeepr@proton.me>
2022-11-08 23:13:42 -0300

    get dmenu directly from the suckless website. added config file for dmenu to resources

--- install-window-manager.sh
+++ install-window-manager.sh
@@ -2,21 +2,28 @@
 
 [ ! -d "$HOME/.local/src" ] && mkdir -p "$HOME/.local/src"
 cd ~/.local/src
+
 # dwm
 git clone https://github.com/rkeepr/dwm.git
 cd "dwm"
 sudo make clean install
 cd ..
-# dmenu
-git clone https://github.com/rkeepr/dmenu.git
-cd "dmenu"
-sudo make clean install
-cd ..
+
 # st
 git clone https://github.com/rkeepr/st.git
 cd "st"
 sudo make clean install
 cd ..
+
+# dmenu
+curl -LO https://dl.suckless.org/tools/dmenu-5.2.tar.gz
+tar -x -f dmenu-5.2.tar.gz
+rm dmenu-5.2.tar.gz
+cd "dmenu-5.2"
+cp ~/arch-dwm/resources/dmenu/config.h .
+sudo make clean install
+cd ..
+
 # dwmblocks
 git clone https://github.com/torrinfail/dwmblocks.git
 cd "dwmblocks"
