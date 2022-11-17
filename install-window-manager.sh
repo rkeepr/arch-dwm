78da5c4a7e056d1fbfb0167ec34d231763936fa4
rkeepr <rkeepr@proton.me>
2022-11-17 20:25:05 -0300

    added curl as a failsafe

--- install-window-manager.sh
+++ install-window-manager.sh
@@ -16,7 +16,7 @@ curl -LO https://dl.suckless.org/tools/dmenu-5.2.tar.gz
 tar -x -f dmenu-5.2.tar.gz
 rm dmenu-5.2.tar.gz
 cd "dmenu-5.2"
-cp ~/arch-dwm/resources/dmenu/config.h .
+cp ~/arch-dwm/resources/dmenu/config.h . || echo 'getting "config.h" from the internet' && curl -LO https://raw.githubusercontent.com/rkeepr/arch-dwm/main/resources/dmenu/config.h
 sudo make clean install
 cd ..
 
@@ -24,5 +24,5 @@ cd ..
 git clone https://github.com/torrinfail/dwmblocks.git
 cd "dwmblocks"
 rm -rf .git
-cp ~/arch-dwm/resources/dwmblocks/blocks.h .
+cp ~/arch-dwm/resources/dwmblocks/blocks.h . || echo 'getting "blocks.h" from the internet' && curl -LO https://raw.githubusercontent.com/rkeepr/arch-dwm/main/resources/dwmblocks/blocks.h
 sudo make clean install
