df5cbc9387635dd284602efe789fcd8a3f10aecc
rkeepr <113490552+rkeepr@users.noreply.github.com>
2023-01-03 02:39:24 -0300

    Update sb-home-left.sh

--- resources/local/bin/sb-home-left.sh
+++ resources/local/bin/sb-home-left.sh
@@ -4,4 +4,4 @@ df_out="$(df -h /home /mnt/storage)"
 home_part="$(echo $df_out | awk '{print $11}')"
 storage_part="$(echo $df_out | awk '{print $17}')"
 
-echo "[ "$HOME_PART"  "$STORAGE_PART"]"
+echo "[ "$home_part"  "$storage_part"]"
