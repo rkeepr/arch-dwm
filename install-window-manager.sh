ff85f84e52f30538f27db0e1fc346f2c4b554a4b
rkeepr <rkeepr@proton.me>
2023-01-04 23:09:24 -0300

    Update install-window-manager.sh

--- install-window-manager.sh
+++ install-window-manager.sh
@@ -2,29 +2,29 @@
 
 ordir=$(pwd)
 
-mkdir -p "$HOME/.local/src" && cd ~/.local/src
+mkdir -p "$HOME/.local/suckless" && cd ~/.local/suckless
 
 # dwm and st
 for s in dwm st
 do
-	 git clone https://github.com/rkeepr/$s.git
-	 cd $s
-	 sudo make clean install
-	 cd ..
+	[ ! -f $s.zip ] && curl -L https://github.com/rkeepr/$s/archive/refs/heads/main.zip > $s.zip
+	unzip -uo $s.zip
+	cd $s-main
+	sudo make clean install
+	cd ..
 done
 
 # dmenu
-curl -LO https://dl.suckless.org/tools/dmenu-5.2.tar.gz
+[ ! -f dmenu-5.2.tar.gz ] && curl -LO https://dl.suckless.org/tools/dmenu-5.2.tar.gz
 tar -x -f dmenu-5.2.tar.gz
-rm dmenu-5.2.tar.gz
 cd "dmenu-5.2"
 cp $ordir/resources/dmenu/config.h . || echo 'getting "config.h" from the internet' && curl -LO https://raw.githubusercontent.com/rkeepr/arch-dwm/main/resources/dmenu/config.h
 sudo make clean install
 cd ..
 
 # dwmblocks
-git clone https://github.com/torrinfail/dwmblocks.git
-cd "dwmblocks"
-rm -rf .git
+[ ! -f dwmblocks.zip ] && curl -L https://github.com/torrinfail/dwmblocks/archive/refs/heads/master.zip > dwmblocks.zip
+unzip -uo dwmblocks.zip
+cd "dwmblocks-master"
 cp $ordir/resources/dwmblocks/blocks.h . || echo 'getting "blocks.h" from the internet' && curl -LO https://raw.githubusercontent.com/rkeepr/arch-dwm/main/resources/dwmblocks/blocks.h
 sudo make clean install
