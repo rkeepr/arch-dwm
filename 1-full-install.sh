3c11b6a2555c680787f7c7331d4909656b181503
rkeepr <rkeepr@proton.me>
2023-01-05 01:22:04 -0300

    Update 1-full-install.sh

--- 1-full-install.sh
+++ 1-full-install.sh
@@ -6,7 +6,7 @@
 ### install aur helper
 which yay && echo "aur helper already installed, skipping." || ./install-aur-helper.sh
 # install some packages from the aur
-./install-aur-packages.sh
+pacman -Qm - < resources/aur-packages && echo "aur packages already installed, skipping" || ./install-aur-packages.sh
 
 ### suckless
 which st dwm dmenu dwmblocks && echo "window manager already installed, skipping." || ./install-window-manager.sh
