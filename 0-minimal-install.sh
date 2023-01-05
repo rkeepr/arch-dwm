4918565de148e75bba79cf57ba50ba384260f49f
rkeepr <rkeepr@proton.me>
2023-01-05 01:05:51 -0300

    Update 0-minimal-install.sh

--- 0-minimal-install.sh
+++ 0-minimal-install.sh
@@ -4,10 +4,10 @@
 sudo pacman -S --needed --noconfirm base-devel xorg-server xorg-xinit libxinerama firefox lf thunar thunar-archive-plugin thunar-volman thunar-media-tags-plugin tumbler gvfs xarchiver unzip zip unrar man-db htop ttf-hack-nerd ttf-liberation
 
 ### install aur helper
-./install-aur-helper.sh
+which yay && echo "aur helper already installed, skipping." || ./install-aur-helper.sh
 
 ### suckless
-./install-window-manager.sh
+which st dwm dmenu dwmblocks && echo "window manager already installed, skipping." || ./install-window-manager.sh
 
 ### copy and modify the dotfiles
 ./copy-dotfiles.sh
