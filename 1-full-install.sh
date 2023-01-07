54d078746b5943bd08894e039cd9ee9b908b0c24
rkeepr <rkeepr@proton.me>
2023-01-07 17:07:46 -0300

    Update 1-full-install.sh

--- 1-full-install.sh
+++ 1-full-install.sh
@@ -1,15 +1,18 @@
 #!/bin/sh
 
+quit=$(echo "installation failed, try again." && exit)
+
 ### full install
-./install-packages.sh
+./install-packages.sh || $quit
 
 ### install aur helper
-which yay && echo "aur helper already installed, skipping." || ./install-aur-helper.sh
-# install some packages from the aur
-./install-aur-packages.sh
+which yay && echo "aur helper already installed, skipping." || ./install-aur-helper.sh || $quit
+
+### install some packages from the aur
+./install-aur-packages.sh || $quit
 
 ### suckless
-which st dwm dmenu dwmblocks && echo "window manager already installed, skipping." || ./install-window-manager.sh
+which st dwm dmenu dwmblocks && echo "window manager already installed, skipping." || ./install-window-manager.sh || $quit
 
 ### copy and modify the dotfiles
 ./copy-dotfiles.sh
