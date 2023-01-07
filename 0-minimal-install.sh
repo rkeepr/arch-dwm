33956175bbec7c402e13b042d7f28960856da65a
rkeepr <rkeepr@proton.me>
2023-01-07 17:04:26 -0300

    Update 0-minimal-install.sh

--- 0-minimal-install.sh
+++ 0-minimal-install.sh
@@ -1,13 +1,14 @@
 #!/bin/sh
 
+quit=$(echo "installation failed, try again." && exit)
+
 ### basic install
-sudo pacman -S --needed --noconfirm $(awk '/#M/{print $1}' resources/pkg-list)
+sudo pacman -S --needed --noconfirm $(awk '/#M/{print $1}' resources/pkg-list) || $quit
 
 ### install aur helper
-which yay && echo "aur helper already installed, skipping." || ./install-aur-helper.sh
-
+which yay && echo "aur helper already installed, skipping." || ./install-aur-helper.sh || $quit
 ### suckless
-which st dwm dmenu dwmblocks && echo "window manager already installed, skipping." || ./install-window-manager.sh
+which st dwm dmenu dwmblocks && echo "window manager already installed, skipping." || ./install-window-manager.sh || $quit
 
 ### copy and modify the dotfiles
 ./copy-dotfiles.sh
