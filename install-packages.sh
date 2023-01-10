bf155276ae1b4830aa0024bdc579a8266e06f237
rkeepr <rkeepr@proton.me>
2023-01-10 20:48:33 -0300

    Update install-packages.sh

--- install-packages.sh
+++ install-packages.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 
 ### full install
-sudo pacman -S --needed --noconfirm --color=auto $(awk '!/#[A,a-z, ]/{print $1}' resources/pkg-list)
+sudo pacman -S --needed --noconfirm --color=auto $(grep -E '^[a-z]' resources/pkg-list | awk '!/#A/{print $1}')
