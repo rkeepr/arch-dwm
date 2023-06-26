5a901393ddfe825fa794e5ad30a64aba665e817f
rkeepr <rkeepr@proton.me>
2023-06-26 06:49:46 -0300

    Update pv.sh

--- resources/config/lf/pv.sh
+++ resources/config/lf/pv.sh
@@ -2,7 +2,10 @@
 
 case "$(file --dereference --brief --mime-type "$1")" in
 	image/*|video/*|audio/*) mediainfo "$1" ;;
-	text/*|*.xml|application/json) bat -f "$1" ;;
+	text/*|*/xml|application/json) bat -f "$1" ;;
 	application/pdf) pdftotext "$1" - ;;
-	application/*) atool --list -- "$1" ;;
+	application/gzip) tar tf "$1" ;;
+	application/zip) unzip -l "$1" ;;
+	application/x-rar) unrar l "$1" ;;
+	application/x-7z-compressed) 7z l "$1" ;;
 esac
