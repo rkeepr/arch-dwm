0001dbed23a85e170d3a7d1661d0becc8db47025
rkeepr <rkeepr@proton.me>
2024-08-12 18:49:11 -0300

    Update sb-disk-space-aval.sh

--- resources/local/bin/sb-disk-space-aval.sh
+++ resources/local/bin/sb-disk-space-aval.sh
@@ -1,8 +1,7 @@
 #!/bin/sh
 
-df_out="$(df -h /home /mnt/storage /mnt/torrent)"
+df_out="$(df -h /home /mnt/storage)"
 home_part="$(echo $df_out | awk '{print $11}')"
 storage_part="$(echo $df_out | awk '{print $17}')"
-torrent_part="$(echo $df_out | awk '{print $23}')"
 
-echo "[ "$home_part" 󰋊 "$storage_part" 󰇚 "$torrent_part"]"
+echo "[ "$home_part" 󰋊 "$storage_part"]"
