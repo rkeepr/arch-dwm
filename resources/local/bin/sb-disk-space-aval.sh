35e2ce2d2d37a006f7dce7782c24dfeb2b095b50
rkeepr <rkeepr@proton.me>
2025-07-22 22:18:48 -0300

    Update sb-disk-space-aval.sh

--- resources/local/bin/sb-disk-space-aval.sh
+++ resources/local/bin/sb-disk-space-aval.sh
@@ -1,7 +1,8 @@
 #!/bin/sh
 
-df_out="$(df -h /home /mnt/storage)"
+df_out="$(df -h /home /mnt/storage /mnt/storage2)"
 home_part="$(echo $df_out | awk '{print $11}')"
 storage_part="$(echo $df_out | awk '{print $17}')"
+storage2_part="$(echo $df_out | awk '{print $23}')"
 
-echo "[ "$home_part" 󰋊 "$storage_part"]"
+echo "[ "$home_part" 󰋊 "$storage_part" 󰋊 "$storage2_part"]"
